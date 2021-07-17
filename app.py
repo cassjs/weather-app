29942223aba56a9f4167acf4aac7cc614915735b
cassjsdev <cassjes.m@gmail.com>
2021-07-17 15:42:07 -0700

    specified  objects

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    instance_relative_config= False,
+    template_folder= "templates",
+    static_folder= "static"
+    )
 
 @app.route("/")
 def index():
