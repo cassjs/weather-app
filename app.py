481ec69d3ab17c688aa7f7f92eed75f79f01a157
cassjsdev <cassjes.m@gmail.com>
2021-07-22 23:20:00 -0700

    added database

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import requests
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weatherdb.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cityname = db.Column(db.String(50), nullable=False)
+    
+db.create_all()
 
 @app.route('/')
 def index():
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=734bd3e5ad848030de098b13791517e1'
     city = 'Beaverton'
     
     r = requests.get(url.format(city)).json()
@@ -26,3 +36,7 @@ def index():
     print(weather)
     
     return render_template('base.html', weather=weather)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
